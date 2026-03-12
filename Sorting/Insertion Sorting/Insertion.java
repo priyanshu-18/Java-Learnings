1262271223430f42f3779448787acf42c6bd97f8
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-03-12 20:02:42 +0530

    solved some leetcode question and learned the insertion sorting again

--- Sorting/Insertion Sorting/Insertion.java
+++ Sorting/Insertion Sorting/Insertion.java
@@ -5,15 +5,19 @@ public class Insertion {
         public static void main(String[] args) {
             int arr[] = {13,46,24,52,20,9};
             int n = arr.length;
-            for(int i=0;i<n-1;i++){
+            for(int i=1;i<n;i++){
                 int current = arr[i];
                 int j = i-1;
 
                 while(j>=0 && arr[j]>current){
-                    arr[j+1]=current;
+                    arr[j+1]=arr[j];
                     j--;
                 }
                 arr[j+1] =current;
+            }
+        
+            for(int i : arr){
+                System.out.print(i+" ");
             }
         
         }
