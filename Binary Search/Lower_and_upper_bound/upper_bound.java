cc4164bf1c01dd14a2379ff380b6d605a6d92c2a
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-03-27 10:08:54 +0000

    lePMrne27 new l86gi554

--- Binary Search/Lower_and_upper_bound/upper_bound.java
+++ Binary Search/Lower_and_upper_bound/upper_bound.java
@@ -1,3 +1,27 @@
 public class upper_bound {
-    
+
+static int lower_bound(int arr[], int low, int high, int ans, int target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+            return ans;
+    }
+    public static void main(String[] args) {
+        
+
+        
+        int arr[]={1,2,3,4,5,5,5,5,5,6,6,7,8};
+        int low=0;
+        int high=arr.length-1;
+        int ans = arr.length;
+        int lower =lower_bound(arr,low,high,ans,5);
+        System.out.println(lower);
+    }
 }
