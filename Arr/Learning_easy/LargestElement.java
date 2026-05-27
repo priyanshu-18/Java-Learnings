13ae6d27432b3d2d9d8503664c4a5a3e417d7942
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-05-27 08:55:42 +0000

    lePMrne27 new l147gi342

--- Arr/Learning_easy/LargestElement.java
+++ Arr/Learning_easy/LargestElement.java
@@ -4,17 +4,20 @@ public class LargestElement {
     public static void main(String[] args) {
        
         //methods 1(brute)
-        
-        int arr[]={2,3,4,5,6,4,3,2,7};
-        int largest= arr[0];
-        
-        for(int i = 1 ;i < arr.length;i++){
-            if(arr[i]>largest){
-                largest = arr[i];
-            }
-        }
-        System.out.println(largest);
 
+        // int arr[]={2,3,4,5,6,4,3,2,7};
+        // int largest= arr[0];
+        
+        // for(int i = 1 ;i < arr.length;i++){
+        //     if(arr[i]>largest){
+        //         largest = arr[i];
+        //     }
+        // }
+        // System.out.println(largest);
+
+
+
+        
 
     }
     
