6165f35eaae28b277ea3c293963b2cea491fe72b
priyanshu-18 <priyanshuphotos18@gmail.com>
2026-05-27 11:05:49 +0000

    lePMrne27 new l147gi349

--- Arr/Learning_easy/LargestElement.java
+++ Arr/Learning_easy/LargestElement.java
@@ -39,15 +39,15 @@ public class LargestElement {
         int Smallest = arr[0]; int SSmallest = Integer.MAX_VALUE;
 
         for(int i=1;i<arr.length;i++){
-            if(arr[i]>largest){
-                Slargest = largest;
-                largest = arr[i];
+            if(arr[i]>Smallest){
+                SSmallest = Smallest;
+                Smallest = arr[i];
             }
-            else if(arr[i]<largest && arr[i]>Slargest){
-                Slargest=arr[i];
+            else if(arr[i]<Smallest && arr[i]>SSmallest){
+                SSmallest=arr[i];
             }
         }
-        System.out.println(Slargest);
+        System.out.println(SSmallest);
 
 
 
